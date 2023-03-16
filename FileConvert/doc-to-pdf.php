4e4ffe9b6b0237f43cf37914821b77559a3694d2
ESWARARAO BETHA <126596692+Royal-Code-Master@users.noreply.github.com>
2023-03-16 22:13:59 +0530

    Add files via upload

---
+++ FileConvert/doc-to-pdf.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>DOC To PDF Conversion Results</title>
+</head>
+
+<body>
+
+    <?php
+
+    // Note: If you have input files large than 200kb we highly recommend to check "async" mode example.
+
+    // Get submitted form data
+    $apiKey = "stdintern@gmail.com_7623a22c60a0f5a95d17cf6d0fdd84d1a998a438eda1aa3e624614b3875f27083a444b1f"; // The authentication key (API Key). Get your own by registering at https://app.pdf.co
+
+
+    // 1. RETRIEVE THE PRESIGNED URL TO UPLOAD THE FILE.
+    // * If you already have the direct PDF file link, go to the step 3.
+
+    // Create URL
+    $url = "https://api.pdf.co/v1/file/upload/get-presigned-url" .
+        "?name=" . urlencode($_FILES["file"]["name"]) .
+        "&contenttype=application/octet-stream";
+
+    // Create request
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("x-api-key: " . $apiKey));
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    // Execute request
+    $result = curl_exec($curl);
+
+    if (curl_errno($curl) == 0) {
+        $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        if ($status_code == 200) {
+            $json = json_decode($result, true);
+
+            if (!isset($json["error"]) || $json["error"] == false) {
+                // Get URL to use for the file upload
+                $uploadFileUrl = $json["presignedUrl"];
+                // Get URL of uploaded file to use with later API calls
+                $uploadedFileUrl = $json["url"];
+
+                // 2. UPLOAD THE FILE TO CLOUD.
+
+                $localFile = $_FILES["file"]["tmp_name"];
+                $fileHandle = fopen($localFile, "r");
+
+                curl_setopt($curl, CURLOPT_URL, $uploadFileUrl);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, array("content-type: application/octet-stream"));
+                curl_setopt($curl, CURLOPT_PUT, true);
+                curl_setopt($curl, CURLOPT_INFILE, $fileHandle);
+                curl_setopt($curl, CURLOPT_INFILESIZE, filesize($localFile));
+
+                // Execute request
+                curl_exec($curl);
+
+                fclose($fileHandle);
+
+                if (curl_errno($curl) == 0) {
+                    $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+                    if ($status_code == 200) {
+                        // 3. CONVERT UPLOADED DOC FILE TO PDF
+
+                        DocToPdf($apiKey, $uploadedFileUrl, '');
+                    } else {
+                        // Display request error
+                        echo "<p>Status code: " . $status_code . "</p>";
+                        echo "<p>" . $result . "</p>";
+                    }
+                } else {
+                    // Display CURL error
+                    echo "Error: " . curl_error($curl);
+                }
+            } else {
+                // Display service reported error
+                echo "<p>Error: " . $json["message"] . "</p>";
+            }
+        } else {
+            // Display request error
+            echo "<p>Status code: " . $status_code . "</p>";
+            echo "<p>" . $result . "</p>";
+        }
+
+        curl_close($curl);
+    } else {
+        // Display CURL error
+        echo "Error: " . curl_error($curl);
+    }
+
+    function DocToPdf($apiKey, $uploadedFileUrl, $pages)
+    {
+        // Create URL
+        $url = "https://api.pdf.co/v1/pdf/convert/from/doc";
+
+        // Prepare requests params
+        $parameters = array();
+        $parameters["url"] = $uploadedFileUrl;
+        $parameters["pages"] = $pages;
+
+        // Create Json payload
+        $data = json_encode($parameters);
+
+        // Create request
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("x-api-key: " . $apiKey, "Content-type: application/json"));
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+        // Execute request
+        $result = curl_exec($curl);
+
+        if (curl_errno($curl) == 0) {
+            $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+            if ($status_code == 200) {
+                $json = json_decode($result, true);
+
+                if (!isset($json["error"]) || $json["error"] == false) {
+                    $resultFileUrl = $json["url"];
+
+                    // Display link to the file with conversion results
+
+                    echo "<div><h2>Conversion Result:</h2><a href='" . $resultFileUrl . "' target='_blank'>" . $resultFileUrl . "</a></div>";
+                } else {
+                    // Display service reported error
+                    echo "<p>Error: " . $json["message"] . "</p>";
+                }
+            } else {
+                // Display request error
+                echo "<p>Status code: " . $status_code . "</p>";
+                echo "<p>" . $result . "</p>";
+            }
+        } else {
+            // Display CURL error
+            echo "Error: " . curl_error($curl);
+        }
+
+        // Cleanup
+        curl_close($curl);
+    }
+
+    ?>
+
+</body>
+
+</html>
